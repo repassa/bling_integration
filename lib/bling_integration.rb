0108a7e1d447144664778a93d55b5b17ae43d1e7
Emerson de Assis <emerson.silva@repassa.com.br>
2023-02-28 15:00:18 -0300

    Adicionado class para gerar NFE

--- lib/bling_integration.rb
+++ lib/bling_integration.rb
@@ -7,6 +7,7 @@ require 'json'
 require_relative 'bling_integration/http/base_client'
 require_relative 'bling_integration/client'
 require_relative 'bling_integration/nfe_client'
+require_relative 'bling_integration/nfe'
 require_relative 'bling_integration/version'
 require_relative 'bling_integration/xml_builder'
 
