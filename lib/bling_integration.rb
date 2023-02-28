e76dfed01a4da34d7815411df2d48d0e714bc6a0
Emerson de Assis <emerson.silva@repassa.com.br>
2023-02-28 12:22:18 -0300

    Adicionado class que contem os métodos para envio da nota fiscal
    Nesta class são inseridas os métodos responsáveis apenas por coisas
    relacionado a nota fiscal

--- lib/bling_integration.rb
+++ lib/bling_integration.rb
@@ -2,6 +2,7 @@
 
 require_relative 'bling_integration/http/base_client'
 require_relative 'bling_integration/client'
+require_relative 'bling_integration/nfe_client'
 require_relative 'bling_integration/version'
 require_relative 'bling_integration/xml_builder'
 
