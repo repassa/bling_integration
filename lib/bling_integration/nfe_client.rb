6ebebc6054b4365d436b6d70cd8805821e6dbd06
Emerson de Assis <emerson.silva@repassa.com.br>
2023-03-06 15:09:15 -0300

    Adicionado métodos de envio e busca de nota
    Adiciondo os métodos para enviar o RPS para o Ginfes, gerando a nota de serviço
    Adicionado método para buscar informações na nota passando o número do RPS

--- lib/bling_integration/nfe_client.rb
+++ lib/bling_integration/nfe_client.rb
@@ -9,5 +9,22 @@ module BlingIntegration
       }
       default_request('notaservico', params: params)
     end
+
+    def generate_nfse(numero_rps, serie, api_key)
+      params = {
+        apikey: api_key,
+        number: numero_rps,
+        serie:  serie
+      }
+      default_request('notaservico', params: params)
+    end
+
+    def nfse(numero_rps, api_key)
+      params = {
+        apikey: api_key,
+        number: numero_rps
+      }
+      default_request("notaservico/#{numero_rps}", params: params, http_method: :get)
+    end
   end
 end
